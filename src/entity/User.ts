3fbc1ff89b9175846dad6433b6afda45c7ea1943
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-01-30 16:01:40 -0300

    build:typeorm

---
+++ src/entity/User.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  birth: number;
+
+  @Column()
+  cep: number;
+
+  @Column()
+  adress: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  password: string;
+}
